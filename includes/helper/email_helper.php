0b54e683f6823f452f2f37488d699e9150fc4df6
anykraus <anykraus@users.noreply.github.com>
2017-01-03 23:37:31 +0100

    use helfer.jcf-fruehjahrssymposium.de E-Mail as from
    fixes issue #23 "E-Mail from is still @engelsystem.de"

--- includes/helper/email_helper.php
+++ includes/helper/email_helper.php
@@ -16,7 +16,7 @@ function engelsystem_email_to_user($recipient_user, $title, $message, $not_if_it
 }
 
 function engelsystem_email($address, $title, $message) {
-  return mail($address, $title, $message, "Content-Type: text/plain; charset=UTF-8\r\nFrom: Helpersystem <noreply@engelsystem.de>");
+  return mail($address, $title, $message, "Content-Type: text/plain; charset=UTF-8\r\nFrom: Helfersystem <noreply@helfer.jcf-fruehjahrssymposium.de>");
 }
 
 ?>
